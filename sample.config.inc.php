ccf45e6738e8d27b88e61ab440de7589c0d15dd0
ekanshpreet <ekanshpreet@gmail.com>
2012-08-18 03:42:16 +0530

    Sample Configuration File

---
+++ sample.config.inc.php
@@ -0,0 +1,17 @@
+<?php
+
+define("APP_TITLE", "Data Portraits - Berkman Center for Internet and Society");
+
+define("CONSUMER_KEY", "YOU_KEY_HERE");
+define("CONSUMER_KEY_SECRET", "YOUR_KEY_SECRET_HERE");
+
+// e.g If the URL to your install is http://localhost/dataportraits, put that as your SITE ROOT PATH
+define("SITE_ROOT_PATH", "YOUR_SITE_ROOT_PATH_HERE");
+define("DEBUG", false);
+define("CACHE_PAGES", false);
+
+
+define("DB_HOST","YOUR_DB_HOST");
+define("DB_USER","YOUR_DB_USER");
+define("DB_PASSWORD","YOUR_DB_PASSWORD");
+define("DB_NAME","YOUR_DB_NAME");
