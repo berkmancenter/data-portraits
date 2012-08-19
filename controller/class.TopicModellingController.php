c3fef3f30fd5ef6b3dc31929a4bf422b4d5496a5
ekanshpreet <ekanshpreet@gmail.com>
2012-08-20 02:12:33 +0530

    Topic Modelling via Java

--- controller/class.TopicModellingController.php
+++ controller/class.TopicModellingController.php
@@ -30,6 +30,7 @@
  */
 require_once(ROOT_PATH."/controller/class.DPController.php");
 require_once(ROOT_PATH."/model/class.TopicModel.php");
+require_once(ROOT_PATH."/model/class.StatusProcessing.php");
 
 class TopicModellingController extends DPController {
     
@@ -39,6 +40,11 @@ class TopicModellingController extends DPController {
         } else {
 	    $statuses = json_decode($_POST['statuses']);
         }
+	$this->performTopicModellingJava($statuses);
+	//Old Stuff
+        //$this->addToView('statuses', $statuses);
+        //$this->setViewTemplate('topics.tpl');
+        //return $this->generateView();
         $model = new TopicModel();
 	$result = $model->analyse($statuses);
 	$num = "var num = ".json_encode($result['num']).";";
@@ -49,11 +55,111 @@ class TopicModellingController extends DPController {
 	$this->addToView('tweets', $tweets);
 	$this->addToView('topic_text', $topic_text);
 	$this->addToView('topic_text_values', $topic_text_values);
-        //$this->addToView('statuses', $statuses);
-        //$this->setViewTemplate('topics.tpl');
-        //return $this->generateView();
 	$this->setViewTemplate('topics_new.tpl');
 	return $this->generateView();
     }
     
+    private function performTopicModellingJava($statuses, $topic_count = 10) {
+	$documents = $this->extractDocuments($statuses);
+	$count = count($documents);
+	$username = $_SESSION['access_token']['screen_name'];
+	$filename = ROOT_PATH."/data/topics/".$username.".txt";
+	$hashmap = $this->generateHashMap($documents);
+	$vocab_count = count($hashmap);
+	$mapped_docs = $this->mapDocuments($documents, $hashmap);
+	$this->putData($filename, $mapped_docs);
+	$path = ROOT_PATH."/bin/topics/LDA.jar";
+	$cmd = "java -jar $path $count $vocab_count $filename $topic_count";
+	exec($cmd, $output);
+	foreach ($output as $line) {
+	    print_r($line);
+	    echo "<br/>";
+	}
+	echo "<br/><br/>";
+	$topics = array();
+        for ($i = 0; $i < $topic_count; $i++) {
+            $topics[$i] = array();
+        }
+        $index = 0;
+        $threshold = 10 / $topic_count;
+	foreach ($output as $line) {
+	    $topics_doc = explode(" ", $line);
+            for ($i = 0; $i < $topic_count; $i++) {
+                if ($topics_doc[$i] > $threshold) {
+                    array_push($topics[$i], $index);
+                }
+            }
+            $index++;
+	}
+        for ($i = 0; $i < $topic_count; $i++) {
+            echo "Topic ".($i+1).":<br/>";
+            foreach($topics[$i] as $index) {
+                echo $statuses[$index]->text;
+                echo "<br/>";
+            }
+            echo "<br/><br/>";
+        }
+    }
+    
+    private function mapDocuments($documents, $hashmap) {
+	$mapped = array();
+	foreach ($documents as $document) {
+	    $map = array();
+	    foreach ($document as $word) {
+		array_push($map, $hashmap[$word]);
+	    }
+	    array_push($mapped, $map);
+	}
+	return $mapped;
+    }
+    
+    private function generateHashMap($documents) {
+	$index = 0;
+	$hashmap = array();
+	foreach ($documents as $document) {
+	    foreach ($document as $word) {
+		if (!isset($hashmap[$word])) {
+		    $hashmap[$word] = $index++;
+		}
+	    }
+	}
+	return $hashmap;
+    }
+    
+    private function extractDocuments($statuses) {
+	$documents = array();
+	$words_list = StatusProcessing::findWords($statuses);
+	$stopwords = Utils::getStopWords();
+	foreach ($statuses as $status) {
+	    $document = array();
+	    $tweet = $status->text_processed;
+	    $words = explode(" ", $tweet);
+	    $word_count = count($words);
+	    for ($i = 0; $i < $word_count; $i++) {
+		if (isset($words_list[$words[$i]])) {
+		    array_push($document, $words[$i]);
+		} else if (in_array($words[$i], $stopwords)) {
+		    continue;
+		} else if ($i+1<$word_count && $words_list[$words[$i]." ".$words[$i+1]]) {
+		    array_push($document, $words[$i]." ".$words[$i+1]);
+		    $i += 1;
+		} else if ($i+2<$word_count && $words_list[$words[$i]." ".$words[$i+1]." ".$words[$i+2]]) {
+		    array_push($document, $words[$i]." ".$words[$i+1]." ".$words[$i+2]);
+		    $i += 2;
+		} else if ($i+3<$word_count && $words_list[$words[$i]." ".$words[$i+1]." ".$words[$i+2]." ".$words[$i+3]]) {
+		    array_push($document, $words[$i]." ".$words[$i+1]." ".$words[$i+2]." ".$words[$i+3]);
+		    $i += 3;
+		} else if ($i+4<$word_count && $words_list[$words[$i]." ".$words[$i+1]." ".$words[$i+2]." ".$words[$i+3]." ".$words[$i+4]]) {
+		    array_push($document, $words[$i]." ".$words[$i+1]." ".$words[$i+2]." ".$words[$i+3]." ".$words[$i+4]);
+		    $i += 4;
+		}
+	    }
+	    array_push($documents, $document);
+	}
+	return $documents;
+    }
+    
+    private function putData($filename, $documents) {
+	file_put_contents($filename, json_encode($documents));
+    }
 }
